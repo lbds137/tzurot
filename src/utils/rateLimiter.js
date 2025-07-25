f5b126d47cd28f86e543e4666b0c5ad190bd68fa
Vladlena Costescu <vladimir.costescu@gmail.com>
2025-07-25 14:28:00 -0400

    fix: add ESLint disable comments for default timer implementations

--- src/utils/rateLimiter.js
+++ src/utils/rateLimiter.js
@@ -14,7 +14,9 @@
 const logger = require('../logger');
 
 // Default timer implementations for production use
+// eslint-disable-next-line no-restricted-globals, no-restricted-syntax
 const defaultDelay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+// eslint-disable-next-line no-restricted-globals
 const defaultScheduler = setTimeout;
 
 class RateLimiter {
